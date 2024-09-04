4300ce5edacf708546970060681083dd279057db
emanuelhardwell <emanuelnoe16@gmail.com>
2024-09-03 22:31:19 -0600

    feat: Create component FilterElement

---
+++ src/components/FilterElement/FilterElement.tsx
@@ -0,0 +1,66 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectProps,
+  FormHelperText,
+} from "@mui/material";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+
+interface FilterElementProps<T extends FieldValues> {
+  helperText: string;
+  selectProps?: SelectProps<string | string[]>;
+  control: Control<T>;
+}
+
+const bodyParts = [
+  "back",
+  "cardio",
+  "chest",
+  "lower arms",
+  "lower legs",
+  "neck",
+  "shoulders",
+  "upper arms",
+  "upper legs",
+  "waist",
+];
+
+export const FilterElement = <T extends {}>({
+  helperText,
+  selectProps,
+  control,
+}: FilterElementProps<T>) => {
+  return (
+    <Controller
+      rules={{
+        required: selectProps?.required ? "El nombre es requerido." : undefined,
+      }}
+      control={control}
+      name={(selectProps?.multiple ? "nombres" : "nombre") as Path<T>}
+      render={({ field: { value, onChange } }) => (
+        <FormControl fullWidth>
+          <InputLabel id="age-label">Age</InputLabel>
+          <Select
+            {...selectProps}
+            labelId="age-label"
+            id="age"
+            value={value || (selectProps?.multiple ? [] : "")}
+            label="Age"
+            onChange={(e) => onChange(e.target.value)}
+          >
+            {bodyParts.map((bodyPart) => (
+              <MenuItem key={bodyPart} value={bodyPart}>
+                {bodyPart}
+              </MenuItem>
+            ))}
+          </Select>
+          <FormHelperText error={selectProps?.error}>
+            {helperText}
+          </FormHelperText>
+        </FormControl>
+      )}
+    ></Controller>
+  );
+};

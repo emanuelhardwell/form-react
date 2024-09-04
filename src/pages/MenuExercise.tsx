ee98a00ce48bd0fc7ecb4da59ac6b6e6d8079d96
emanuelhardwell <emanuelnoe16@gmail.com>
2024-09-03 22:26:10 -0600

    feat: Update components MenuExercise andinbstall MUI Icons

--- src/pages/MenuExercise.tsx
+++ src/pages/MenuExercise.tsx
@@ -3,6 +3,7 @@ import { Filters } from "./Filters";
 import { useForm } from "react-hook-form";
 import { AvailabilityFilters } from "../interfaces/ExerciseInterface";
 import { initAvailabilityFilters } from "../utils/constants";
+import { Card, CardContent } from "@mui/material";
 
 export const MenuExercise = () => {
   const formAvailability = useForm<
@@ -14,19 +15,33 @@ export const MenuExercise = () => {
   });
 
   const [loading, setLoading] = useState(false);
+  const { watch } = formAvailability;
+  const filters = watch();
 
   const onSearch = useCallback(async () => {
     setLoading(true);
-  }, []);
+
+    try {
+      console.log("onSearch");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [filters]);
 
   return (
-    <div>
+    <>
       <h1>MenuExercise</h1>
-      <Filters
-        onSearch={onSearch}
-        loading={loading}
-        formAvailability={formAvailability}
-      />
-    </div>
+      <Card>
+        <CardContent>
+          <Filters
+            onSearch={onSearch}
+            loading={loading}
+            formAvailability={formAvailability}
+          />
+        </CardContent>
+      </Card>
+    </>
   );
 };
